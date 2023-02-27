6ca90f6dcc9ac2f84ae756259b30b5b14721fee8
unknown <emilymora1207@gmail.com>
2023-02-27 13:42:34 -0800

    added comments

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,42 @@
+//getting elements from the HTML page
 var searchInput = document.querySelector('.search');
 var searchBtn = document.querySelector('.searchBtn')
 var searchHistoryEl = document.querySelector('#searchHistory');
 var todayForm = document.querySelector('.today');
 var ForecastList = document.querySelector('.forecast');
+var bodyEl = document.querySelector('.body');
 
+//to display the dates for the forecast
 var today = dayjs();
 var searchHistoryList = [];
 
-
+//starts the functions when the searched button was pressed 
 searchBtn.addEventListener('click', getSearchedLocation);
+//starts the functions when a button in the search history was pressed 
+searchHistoryEl.addEventListener('click', getSearchedLocation)
 
 function getSearchedLocation(event) {
     event.preventDefault();
+    //uses both the input from the search and the search history bittons
     var searchedLocation = searchInput.value || event.target.className;
-    // console.log(searchedLocation);
     var locationNoSpace = searchedLocation.replace(' ', '');
     console.log(locationNoSpace);
     GetForecastLocation(locationNoSpace);
-    if(searchHistoryEl === null){
+    //creates the buttons to click on in the search history 
+    if(searchHistoryList === null){
       searchHistoryList = [searchedLocation];
     } else if (!searchHistoryList.includes(searchedLocation)) {
-
       searchHistoryList.unshift(searchedLocation);
+      //puts the search history list into local storage
       localStorage.setItem("searchHistoryList", JSON.stringify(searchHistoryList));
-      storeSearchHistory();
+      displaySearchHistory();
     }
     searchInput.value = "";
 
     console.log(event);
 }
 
-
+//takes the searched location and turned it into lon and lat for api search 
 function GetForecastLocation(location) {
   console.log(location)
     var locationUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=6&appid=e88cd06419645f7fd7b6ea89f17cda0c';
@@ -41,15 +47,13 @@ function GetForecastLocation(location) {
           console.log(data);
           var locationLat = data[0].lat;
           var locationLon = data[0].lon;
-          // console.log(locationLat);
-          // console.log(locationLon);
             GetWeatherForecast(locationLat, locationLon);
         });
       }
     });
   };
 
-
+//takes the lon and lat and returns the weather into for that area 
 function GetWeatherForecast(lat, lon) {
     var latLonForecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&cnt=6&units=imperial&appid=e88cd06419645f7fd7b6ea89f17cda0c';
     fetch(latLonForecastUrl).then(function (response) {
@@ -57,6 +61,7 @@ function GetWeatherForecast(lat, lon) {
         response.json().then(function (data) {
           console.log(data);
             displayForecast(data);
+            displayBackground(data);
         });
       }
     });
@@ -65,10 +70,13 @@ function GetWeatherForecast(lat, lon) {
 // GetWeatherForecast();
 
 function displayForecast(data){
+    //displays the "5 day forecast" heading
     var hidden = document.querySelector('.hidden');
     hidden.style.display= 'initial';
+    //removes the forecast from previous searches
     todayForm.innerHTML = "";
     ForecastList.innerHTML = "";
+    //displays the info for the today element
     var todayDate = today.format('MM/DD/YYYY');
     var todayLocationDate = document.createElement('h2');
     todayForm.appendChild(todayLocationDate);
@@ -89,6 +97,7 @@ function displayForecast(data){
     todayWeatherList.appendChild(todayHumidity);
     todayHumidity.textContent = ('Humidity: ' + data.list[0].main.humidity + ' %');
 
+    //displays the 5 day forecast 
     for (var i=1; i < 6; i++) {
         var setDate = dayjs().add(i, 'day');
         var dateFormat = setDate.format('MM/DD/YYYY');
@@ -114,14 +123,29 @@ function displayForecast(data){
     }
 };
 
-function storeSearchHistory(){
+//changes the background image based on the type of weather in the searched location
+function displayBackground(data) {
+  if (data.list[0].weather[0].main === 'Clouds') {
+    document.body.style.backgroundImage = "url('./assets/nikhil-dafare-4F1pwplJ18w-unsplash.jpg')";
+  } else if (data.list[0].weather[0].main === 'Rain') {
+    document.body.style.backgroundImage = "url('./assets/pexels-pixabay-414659.jpg')";
+  } else if (data.list[0].weather[0].main === 'Snow') {
+    document.body.style.backgroundImage = "url('./assets/snow.jpg')";
+  } else if (data.list[0].weather[0].main === 'Thunderstorm') {
+    document.body.style.backgroundImage = "url('./assets/thunder.jpg')";
+  } else if (data.list[0].weather[0].main === 'Drizzle') {
+    document.body.style.backgroundImage = "url('./assets/drizzle.jpg')";
+  }else {
+    document.body.style.backgroundImage = "url('./assets/james-butterly-iEPgfWwTpuM-unsplash.jpg')";
+  } 
+}
 
-
+//displays the search history
+function displaySearchHistory(){
   const getSearchHistory = JSON.parse(localStorage.getItem("searchHistoryList"));
   searchHistoryList = getSearchHistory;
   console.log(getSearchHistory);
   if (getSearchHistory !== null) {
-
     searchHistoryEl.innerHTML = '';
     for(i=0; i<getSearchHistory.length; i++) {
       var historyBtn = document.createElement('button');
@@ -131,11 +155,8 @@ function storeSearchHistory(){
   }
 }
 }
-//   GetWeatherForecast()
-searchHistoryEl.addEventListener('click', getSearchedLocation)
 
-storeSearchHistory();
-
-
+//presents the search history on page load 
+displaySearchHistory();
 
 
